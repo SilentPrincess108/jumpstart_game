0f5240fc2e8e7d8c171ea9698d3033c105f35543
Rosan <rosan.syal@gmail.com>
2025-08-02 18:44:37 -0400

    Made sure that popup appeared at mouse pointer position (not perfect yet)

--- scripts/pop_ups.gd
+++ scripts/pop_ups.gd
@@ -1,7 +1,8 @@
 extends Control
 
-func ItemPopUp(slot, item):
-	%ItemPopUp.popup()
+func ItemPopUp(object, item):
+	var mouse_pos = get_viewport().get_mouse_position()
+	%ItemPopUp.popup(Rect2i(mouse_pos, %ItemPopUp.size))
 
 func HideItemPopup():
 	%ItemPopUp.hide()
