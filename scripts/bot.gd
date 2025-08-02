414705d7c047b188815097e2699fae6ddaa3712e
Rosan <rosan.syal@gmail.com>
2025-08-02 16:40:50 -0400

    Added item info pop up, not proper

--- scripts/bot.gd
+++ scripts/bot.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +8,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_mouse_entered() -> void:
+	PopUps.ItemPopUp(null, null)
+
+
+
+func _on_mouse_exited() -> void:
+	PopUps.HideItemPopup()
---
+++ scripts/pop_ups.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func ItemPopUp(slot, item):
+	%ItemPopUp.popup()
+
+func HideItemPopup():
+	%ItemPopUp.hide()
