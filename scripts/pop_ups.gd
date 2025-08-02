2e03836e2b914546450a7c70bfa1d7daf120afd3
Rosan <rosan.syal@gmail.com>
2025-08-02 19:45:23 -0400

    Added modifiable item name and desc

--- scripts/pop_ups.gd
+++ scripts/pop_ups.gd
@@ -1,7 +1,13 @@
 extends Control
 
-func ItemPopUp(object, item):
+@onready var item: Label = %Name
+@onready var info: Label = %Desc
+
+
+func ItemPopUp(item_name, desc):
 	var mouse_pos = get_viewport().get_mouse_position()
+	item.text = item_name
+	info.text = desc
 	%ItemPopUp.popup(Rect2i(mouse_pos, %ItemPopUp.size))
 
 func HideItemPopup():
