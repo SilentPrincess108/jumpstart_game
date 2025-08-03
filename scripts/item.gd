cdd5949aaca45e341b7865af0c8998fd919fbc3d
Rosan <rosan.syal@gmail.com>
2025-08-03 13:55:48 -0400

    Added possess feature
    Logic works but not as intended, still need to remove main player sprite when possessed and for some reason, all items are possessed when only 1 item is clicked

---
+++ scripts/item.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+#var to connect to area2d for hover detect
+@onready var pop_ups: Control = get_node("/root/PopUps")
+@onready var hover_area: Area2D = $HoverArea
+
+#vars for item data
+@export var item_name: String = "Name"
+@export var item_desc: String = "This is an item"
+
+#var for whether this object is possessed or not
+var is_possessed: bool = false
+
+#var for speed and gravity
+const SPEED = 300.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready() -> void:
+	hover_area.mouse_entered.connect(_on_mouse_entered)
+	hover_area.mouse_exited.connect(_on_mouse_exited)
+	pop_ups.item_possessed.connect(possessed)
+	
+func _physics_process(delta: float) -> void:
+	if is_possessed:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		get_input(Input, delta)
+
+func _on_mouse_entered() -> void:
+	PopUps.ItemPopUp(item_name, item_desc, self)
+
+func _on_mouse_exited() -> void:
+	PopUps.HideItemPopup()
+
+func possessed():
+	is_possessed = true
+
+func get_input(event, delta):
+	#Check for movement
+	if event.is_action_pressed("left"):
+		position.x -= SPEED * delta
+	elif event.is_action_pressed("right"):
+		position.x += SPEED * delta
+	move_and_slide()
--- scripts/pop_ups.gd
+++ scripts/pop_ups.gd
@@ -2,13 +2,22 @@ extends Control
 
 @onready var item: Label = %Name
 @onready var info: Label = %Desc
+var current_target = null #to track hovered object
+signal item_possessed
 
-
-func ItemPopUp(item_name, desc):
+func ItemPopUp(item_name, desc, target):
 	var mouse_pos = get_viewport().get_mouse_position()
 	item.text = item_name
 	info.text = desc
+	current_target = target
 	%ItemPopUp.popup(Rect2i(mouse_pos, %ItemPopUp.size))
 
 func HideItemPopup():
 	%ItemPopUp.hide()
+	current_target = null
+
+func _process(_delta: float) -> void:
+	if current_target and Input.is_action_just_pressed("Possess"):
+		print("Item possessed")
+		item_possessed.emit()
+		HideItemPopup()
