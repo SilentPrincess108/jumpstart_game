1019bef900c4871c8ee2fb12acc7e822419d2fb5
Rosan <rosan.syal@gmail.com>
2025-08-10 01:21:57 -0400

    Created and added new light sprite

--- scripts/lightswitch.gd
+++ scripts/lightswitch.gd
@@ -7,5 +7,7 @@ func get_input(event, delta):
 	#Check for key presses
 	if event.is_action_pressed("up"):
 		sprite.texture = on
+		print("Light on")
 	elif event.is_action_pressed("down"):
 		sprite.texture = off
+		print("Light off")
